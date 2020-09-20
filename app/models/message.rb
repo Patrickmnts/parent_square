0f5d381473f1040b264022e87090c120bccaefb7
Patrick Mounts <patrickmnts@gmail.com>
2020-09-20 12:13:55 -0700

    'create message model and spec'

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,11 @@
 class Message < ApplicationRecord
+  validates_presence_of :to_number, :content
 
+  STATUS_INVALID = 'invalid'
+  STATUS_DELIVERED = 'delivered'
+  STATUS_FAILED = 'failed'
+
+  def should_retry?
+    status == STATUS_FAILED
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  it 'has an active model' do
-    expect{ Message.create }.to change { Message.count }.by 1
+  describe 'validations' do
+    it 'responds invalid without a to_number' do
+      message = Message.new(to_number: nil, content: 'hello world')
+      expect(message).to_not be_valid
+    end
+
+    it 'responds invalid without a content' do
+      message = Message.new(to_number: '1112223333', content: nil)
+      expect(message).to_not be_valid
+    end
+
+    it 'responds valid with both to_number and content' do
+      message = Message.new(to_number: '1112223333', content: 'hello world')
+      expect(message).to be_valid
+    end
+  end
+
+  describe 'should_retry?' do
+    it 'returns false if status is invalid' do
+      message = Message.new(to_number: '1112223333', content: 'hello world', status: Message::STATUS_INVALID)
+      expect(message.status).to eq(Message::STATUS_INVALID)
+      expect(message.should_retry?).to eq(false)
+    end
+
+    it 'returns false if status is delivered' do
+      message = Message.new(to_number: '1112223333', content: 'hello world', status: Message::STATUS_DELIVERED)
+      expect(message.status).to eq(Message::STATUS_DELIVERED)
+      expect(message.should_retry?).to eq(false)
+    end
+
+    it 'returns true if status is failed' do
+      message = Message.new(to_number: '1112223333', content: 'hello world', status: Message::STATUS_FAILED)
+      expect(message.status).to eq(Message::STATUS_FAILED)
+      expect(message.should_retry?).to eq(true)
+    end
   end
 end
