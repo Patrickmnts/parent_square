af9376cd686446bc2e373d7afc31f0ca84a4154d
Patrick Mounts <patrickmnts@gmail.com>
2020-09-20 14:00:54 -0700

    begin building out sms_service logic and specs.

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -1,5 +1,5 @@
 class SmsService < ApplicationService
-  attr_reader :to_number, :message
+  attr_reader :message, :provider
 
   def initialize(message)
     if !message.is_a?(Message)
@@ -7,9 +7,29 @@ class SmsService < ApplicationService
     end
 
     @message = message
+    @provider = determine_provider
   end
 
   def call
 
+  end
+
+  private
+  def determine_provider
+    if @message.provider_name.present?
+      PROVIDERS.select { |provider| provider.name != @message.provider_name }.first
+    else
+      select_new_provider
+    end
+  end
+
+  def select_new_provider
+    num = rand(1..10)
+    case
+    when num <= 3
+      PROVIDER_1
+    else
+      PROVIDER_2
+    end
   end
 end
--- spec/services/sms_service_spec.rb
+++ spec/services/sms_service_spec.rb
@@ -12,4 +12,36 @@ RSpec.describe SmsService do
     hash = { to_number: '1112223333', content: 'good bye' }
     expect{ SmsService.new(hash) }.to raise_error(ArgumentError)
   end
+
+  describe 'service methods' do
+    let(:message) { Message.new(to_number: '1112223333', content: 'hello world') }
+    let(:service) { SmsService.new(message) }
+
+    describe 'select_new_provider' do
+      it 'should send to provider 1 more often than provider 2' do
+        providers = []
+        10.times do
+          providers << service.send(:select_new_provider)
+        end
+
+        expect(providers.count(PROVIDER_1)).to be < providers.count(PROVIDER_2)
+      end
+    end
+
+    describe 'determine_provider' do
+      it 'assigns alternative provider if one is already associated with message' do
+        message.update_attributes(provider_name: PROVIDER_1.name)
+        service = SmsService.new(message)
+
+        expect(service.provider).to_not eq(PROVIDER_1)
+        expect(service.provider).to eq(PROVIDER_2)
+      end
+
+      it 'calls select_new_provider if no provider is present' do
+        allow_any_instance_of(SmsService).to receive(:select_new_provider).once
+        message.update_attributes(provider_name: PROVIDER_1.name)
+        SmsService.new(message)
+      end
+    end
+  end
 end
