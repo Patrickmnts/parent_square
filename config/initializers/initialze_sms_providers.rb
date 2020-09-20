80161bb4b3f77bc2bdd3ad35207568395359466d
Patrick Mounts <patrickmnts@gmail.com>
2020-09-20 14:01:15 -0700

    extract sms provider information into an initializer and pull values from .env file

---
+++ config/initializers/initialze_sms_providers.rb
@@ -0,0 +1,11 @@
+PROVIDER_1 = OpenStruct.new({
+  name: 'provider_1',
+  url: ENV['PROVIDER_1_URL']
+})
+
+PROVIDER_2 = OpenStruct.new({
+  name: 'provider_2',
+  url: ENV['PROVIDER_2_URL']
+})
+
+PROVIDERS = [PROVIDER_1, PROVIDER_2]
