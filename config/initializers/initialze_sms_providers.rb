77c97871e780b3680d100549a36e987961dea0ea
Patrick Mounts <patrickmnts@gmail.com>
2020-09-21 20:31:53 -0700

    add provider information back into initializer and set default host names in environment files

--- config/initializers/initialze_sms_providers.rb
+++ config/initializers/initialze_sms_providers.rb
@@ -1,11 +1,11 @@
 PROVIDER_1 = OpenStruct.new({
   name: 'provider_1',
-  url: ENV['PROVIDER_1_URL']
+  url: 'https://jo3kcwlvke.execute-api.us-west-2.amazonaws.com/dev/provider1'
 })
 
 PROVIDER_2 = OpenStruct.new({
   name: 'provider_2',
-  url: ENV['PROVIDER_2_URL']
+  url: 'https://jo3kcwlvke.execute-api.us-west-2.amazonaws.com/dev/provider2'
 })
 
 PROVIDERS = [PROVIDER_1, PROVIDER_2]
